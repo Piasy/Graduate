6e0e1c481c18d34f6acb82172871cba829dab7d9
Piasy <xujianlin@xueba.mobi>
2015-05-18 21:44:22 +0800

    add distwithspeed

--- src/server/src/web/js/common.js
+++ src/server/src/web/js/common.js
@@ -593,70 +593,6 @@ function createHistorySpeedChart(players) {
     return series;
 }
 
-function createTotalDistanceChart(players) {
-    Highcharts.setOptions({
-        global: {
-            useUTC: false
-        }
-    });
-
-    var player_names = [];
-    var s = {};
-    s.showInLegend = false;
-    s.data = [];
-    players.forEach(function (p, i, arr) {
-        player_names.push(p.name);
-        s.data.push(0);
-    });
-    var series_data = [];
-    series_data.push(s);
-
-    var series = [];
-    $('#chart_container').highcharts({
-        chart: {
-            type: 'column',
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
-            events: {
-                load: function() {
-                    series = this.series;
-                }
-            }
-        },
-        title: {
-            text: '跑动总距离'
-        },
-        xAxis: {
-            categories: player_names
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: '距离（米）'
-            }
-        },
-        tooltip: {
-            pointFormat: '<span><b>{point.y:.1f}</b> 米</span>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        exporting: {
-            enabled: false
-        },
-        credits: {
-            enabled: false
-        },
-        series: series_data
-    });
-    return series;
-}
-
 function updateHistorySpeedChartFirstTime(series, players) {
     var ret = {};
     players.forEach(function (p, i, arr) {
@@ -740,10 +676,72 @@ function updateHistorySpeedChart(series, player, remark, totalNumber) {
     return 0;
 }
 
+function createTotalDistanceChart(players) {
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    var player_names = [];
+    var s = {};
+    s.showInLegend = false;
+    s.data = [];
+    players.forEach(function (p, i, arr) {
+        player_names.push(p.name);
+        s.data.push(0);
+    });
+    var series_data = [];
+    series_data.push(s);
+
+    var series = [];
+    $('#chart_container').highcharts({
+        chart: {
+            type: 'column',
+            animation: Highcharts.svg, // don't animate in old IE
+            marginRight: 10,
+            events: {
+                load: function() {
+                    series = this.series;
+                }
+            }
+        },
+        title: {
+            text: '跑动总距离'
+        },
+        xAxis: {
+            categories: player_names
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '距离（米）'
+            }
+        },
+        tooltip: {
+            pointFormat: '<span><b>{point.y:.1f}</b> 米</span>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        exporting: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        series: series_data
+    });
+    return series;
+}
+
 function updateTotalDistanceChartFirstTime(series, players) {
-    console.log("update first time");
     var data = [];
-    //series[0].setData([]);
     players.forEach(function (p, i, arr) {
         /*if (p.selected) {
             series[i].show();
@@ -753,8 +751,6 @@ function updateTotalDistanceChartFirstTime(series, players) {
         var remark = JSON.parse(localStorage.getItem(p.history));
         if (remark != null && remark.curdistance != undefined) {
             data.push(remark.curdistance);
-            //series[0].addPoint([i, remark.curdistance], true, false);
-            console.log("point[" + i + "].y = " + remark.curdistance);
         }
     });
     series[0].setData(data);
@@ -766,7 +762,6 @@ function updateTotalDistanceChart(series, index, remark) {
     } else {
         series.hide();
     }*/
-    console.log("update");
     var points = series.points;
     var ys = [];
     points.forEach(function (p, i, arr) {
@@ -775,6 +770,138 @@ function updateTotalDistanceChart(series, index, remark) {
     if (remark != null && remark.curdistance != undefined && ys.length > index) {
         ys[index] = remark.curdistance;
         series.setData(ys);
+    }
+}
+
+function createDistWithSpeedChart(players) {
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    var player_names = [];
+    players.forEach(function (p, i, arr) {
+        player_names.push(p.name);
+    });
+
+    var series = [];
+    $('#chart_container').highcharts({
+        chart: {
+            type: 'column',
+            animation: Highcharts.svg, // don't animate in old IE
+            marginRight: 10,
+            events: {
+                load: function() {
+                    series = this.series;
+                }
+            }
+        },
+        title: {
+            text: '不同速度跑动距离统计'
+        },
+        xAxis: {
+            categories: player_names
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '距离（米）'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>'+ this.x +'</b><br/>'+
+                    this.series.name +': '+ this.y +'<br/>'+
+                    '总距离：'+ this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+                }
+            }
+        },
+        exporting: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: '高强度跑',
+            data: []
+        }, {
+            name: '冲刺跑',
+            data: []
+        }, {
+            name: '普通跑',
+            data: []
+        }]
+    });
+    return series;
+}
+
+function updateDistWithSpeedChartFirstTime(series, players) {
+    var data1 = [];
+    var data2 = [];
+    var data3 = [];
+    players.forEach(function (p, i, arr) {
+        /*if (p.selected) {
+         series[i].show();
+         } else {
+         series[i].hide();
+         }*/
+        var remark = JSON.parse(localStorage.getItem(p.history));
+        if (remark != null && remark.distwithspeed != null && remark.distwithspeed.length == 3) {
+            data1.push(remark.distwithspeed[0]);
+            data2.push(remark.distwithspeed[1]);
+            data3.push(remark.distwithspeed[2]);
+        }
+    });
+    series[0].setData(data1);
+    series[1].setData(data2);
+    series[2].setData(data3);
+}
+
+function updateDistWithSpeedChart(series, index, remark) {
+    /*if (player.selected) {
+     series.show();
+     } else {
+     series.hide();
+     }*/
+    var points0 = series[0].points;
+    var points1 = series[1].points;
+    var points2 = series[2].points;
+    var ys0 = [];
+    var ys1 = [];
+    var ys2 = [];
+    points0.forEach(function (p, i, arr) {
+        ys0.push(p.y);
+    });
+    points1.forEach(function (p, i, arr) {
+        ys1.push(p.y);
+    });
+    points2.forEach(function (p, i, arr) {
+        ys2.push(p.y);
+    });
+    if (remark != null && remark.distwithspeed != null && remark.distwithspeed.length == 3 && ys0.length > index && ys1.length > index && ys2.length > index) {
+        ys0[index] = remark.distwithspeed[0];
+        ys1[index] = remark.distwithspeed[1];
+        ys2[index] = remark.distwithspeed[2];
+        series[0].setData(ys0);
+        series[1].setData(ys1);
+        series[2].setData(ys2);
     }
 }
 
