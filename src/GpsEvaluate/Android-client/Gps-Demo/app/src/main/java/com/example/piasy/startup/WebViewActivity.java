9356c90a78ea366715cb871ee324af5fee43ebb1
Piasy <xujianlin@xueba.mobi>
2015-06-21 11:08:56 +0800

    adjust UI to mi pad

--- src/GpsEvaluate/Android-client/Gps-Demo/app/src/main/java/com/example/piasy/startup/WebViewActivity.java
+++ src/GpsEvaluate/Android-client/Gps-Demo/app/src/main/java/com/example/piasy/startup/WebViewActivity.java
@@ -2,10 +2,16 @@ package com.example.piasy.startup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 /**
  * Created by piasy on 15/6/9.
@@ -30,6 +36,25 @@ public class WebViewActivity extends Activity {
             public boolean shouldOverrideUrlLoading(WebView view, String url)
             {
                 view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(@NonNull ConsoleMessage consoleMessage) {
+                Log.d("WebViewActivity", consoleMessage.message() + " -- From line "
+                        + consoleMessage.lineNumber() + " of "
+                        + consoleMessage.sourceId());
+
+                return true;
+            }
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     JsResult result) {
+                Log.d("WebViewActivity", message);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                result.confirm();
                 return true;
             }
         });
--- src/server/src/web/js/common.js
+++ src/server/src/web/js/common.js
@@ -593,3 +593,24 @@ function createRealTimeTableItem(player) {
 
     return td;
 }
+
+function detectDeviceSize() {
+  var     s   =   "";
+  s   +=   " \n网页可见区域宽："+   document.body.clientWidth;
+  s   +=   " \n网页可见区域高："+   document.body.clientHeight;
+  s   +=   " \n网页可见区域宽："+   document.body.offsetWidth     +"   (包括边线和滚动条的宽)";
+  s   +=   " \n网页可见区域高："+   document.body.offsetHeight   +"   (包括边线的宽)";
+  s   +=   " \n网页正文全文宽："+   document.body.scrollWidth;
+  s   +=   " \n网页正文全文高："+   document.body.scrollHeight;
+  s   +=   " \n网页被卷去的高："+   document.body.scrollTop;
+  s   +=   " \n网页被卷去的左："+   document.body.scrollLeft;
+  s   +=   " \n网页正文部分上："+   window.screenTop;
+  s   +=   " \n网页正文部分左："+   window.screenLeft;
+  s   +=   " \n屏幕分辨率的高："+   window.screen.height;
+  s   +=   " \n屏幕分辨率的宽："+   window.screen.width;
+  s   +=   " \n屏幕可用工作区高度："+   window.screen.availHeight;
+  s   +=   " \n屏幕可用工作区宽度："+   window.screen.availWidth;
+  s   +=   " \n你的屏幕设置是   "+   window.screen.colorDepth   +"   位彩色";
+  s   +=   " \n你的屏幕设置   "+   window.screen.deviceXDPI   +"   像素/英寸";
+  console.log(s);
+}
